fd7b3b16f69a6a499953433061feaae32492d74e
Nguyen Khac Bao <baonk@zigexn.vn>
2013-05-24 10:32:35 +0700

    Finish sign in, sign out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,44 +13,52 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(:name=> "Example User", :email=> "user@example.com", :password=> "1234", :password_confirmation=>"1234") }
+ before do
+  @user = User.new(name: "Example User", email: "user@example.com",
+   password: "foobar", password_confirmation: "foobar")
+end
 
-  subject { @user }
+subject { @user }
 
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-  it { should respond_to(:authenticate) }
+it { should respond_to(:name) }
+it { should respond_to(:email) }
+it { should respond_to(:password_digest) }
+it { should respond_to(:password) }
+it { should respond_to(:password_confirmation) }
+it { should respond_to(:remember_token) }
+it { should respond_to(:authenticate) }
 
-  #it { should be_valid }
+it { should be_valid }
+describe "remember token" do
+  before { @user.save }
+  its(:remember_token) { should_not be_blank }
+end
 
-  describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
-  end
+describe "when name is not present" do
+  before { @user.name = " " }
+  it { should_not be_valid }
+end
 
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
-  end
+describe "when name is too long" do
+  before { @user.name = "a" * 51 }
+  it { should_not be_valid }
+end
 
-  describe "when email format is invalid" do
-    it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
-      addresses.each do |invalid_address|
-        @user.email = invalid_address
-        @user.should_not be_valid
-      end
+describe "when email format is invalid" do
+  it "should be invalid" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
+    addresses.each do |invalid_address|
+      @user.email = invalid_address
+      @user.should_not be_valid
     end
   end
+end
 
-  describe "when email format is valid" do
-    it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do |valid_address|
-        @user.email = valid_address
+describe "when email format is valid" do
+  it "should be valid" do
+    addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+    addresses.each do |valid_address|
+      @user.email = valid_address
         #@user.should be_valid
       end
     end
@@ -97,7 +105,7 @@ describe User do
     end
 
     describe "with a password that's too short" do
-      before { @user.password = @user.password_confirmation = "a" * 5 }
+      before { @user.password = @user.password_confirmation = "a" * 1 }
       it { should be_invalid }
     end
   end
