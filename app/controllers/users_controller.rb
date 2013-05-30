59412b6ccc568a2229386c410cb04a8d15465f9f
Nguyen Khac Bao <baonk@zigexn.vn>
2013-05-30 10:33:25 +0700

    Finish edit, signin,signup, edit DONT, error with admin role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,33 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
 
   def show
     @user = User.find(params[:id])
-    #UserMailer.welcome_email(@user).deliver
+  end
 
-      # format.html { redirect_to(@user, :notice => 'User was successfully created.') }
-      # format.json { render :json => @user, :status => :created, :location => @user }
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
     end
+  end
 
-    def new
-      @user = User.new
-    end
-
-    def index
-      @user = User.all
-
-    end
-
-    def update
-      if @user.update_attributes(params[:user])
-        flash[:success] = "Profile updated"
-        sign_in @user
-        redirect_to @user
-      else
-        render 'edit'
-      end
-    end
-
-    def edit
-    #@user = User.find(params[:id])
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -48,33 +45,30 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
 
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
   end
+
+
+  private
+
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    #redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
 end
-
-#   def create
-#     @user = User.new(user_params)
-#     if @user.save
-#       flash[:success] = "Welcome to the Sample App!"
-#       redirect_to @user
-#     else
-#       render 'new'
-#     end
-#   end
-
-#   private
-
-#   def user_params
-#     params.require(:user).permit(:name, :email, :password,
-#      :password_confirmation)
-#   end
-# end
